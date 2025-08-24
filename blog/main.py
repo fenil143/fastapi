c85e3a58283ad447470b2a49d74867fba8eb48ba
fenil143 <mahendrafenil8@gmail.com>
2025-08-24 23:17:01 +0530

    update and delete

--- blog/main.py
+++ blog/main.py
@@ -1,4 +1,4 @@
-from fastapi import Depends, FastAPI # type: ignore
+from fastapi import Depends, FastAPI, Response, status, HTTPException# type: ignore
 from sqlalchemy.orm import Session # type: ignore
 from . import models, schemas, database
 
@@ -27,7 +27,34 @@ def all(db: Session = Depends(get_db)):
     blogs = db.query(models.Blog).all()
     return blogs
 
-@app.get('/blog/{id}')
+@app.get('/blog/{id}', status_code = 200)
 def blog(id, db:Session = Depends(get_db)):
     blog = db.query(models.Blog).filter(models.Blog.id == id).first()
+    if not blog:
+        raise HTTPException(status_code = status.HTTP_404_NOT_FOUND,detail = f'blog with {id} not available.')
+        # Response.status_code = status.HTTP_404_NOT_FOUND
+        # return {'detail':f'blog with {id} not available.'}
+    return blog
+
+@app.delete('/blog/{id}', status_code=status.HTTP_204_NO_CONTENT)
+def destroy(id: int, db: Session = Depends(get_db)):
+    blog = db.query(models.Blog).filter(models.Blog.id == id).first()
+    if not blog:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="Blog not found")
+    db.delete(blog)
+    db.commit()
+    return  # no response body for 204
+
+
+# UPDATE
+@app.put('/blog/{id}', status_code=status.HTTP_202_ACCEPTED)
+def update_blog(id: int, request: schemas.Blog, db: Session = Depends(get_db)):
+    blog = db.query(models.Blog).filter(models.Blog.id == id).first()
+    if not blog:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="Blog not found")
+    
+    blog.title = request.title
+    blog.body = request.body
+    db.commit()
+    db.refresh(blog)
     return blog
